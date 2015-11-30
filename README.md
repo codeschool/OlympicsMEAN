bbe459a9d86e61f1afdbc88359d77030ee2d5b36
Carlos Souza <carloshrsouza@gmail.com>
2015-11-30 17:13:21 -0500

    Initial Code.
    Setup Gulpfile to transpile ES2015 with
    support for new module system.

---
+++ README.md
@@ -0,0 +1,15 @@
+# MEAN Stack
+
+Code for the MEAN Soup to Bits
+
+* MongoDB
+* Express
+* Angular
+* Node
+
+*Requires MongoDB server running*
+
+## Developing
+
+* `npm install` to resolve dependencies
+* `npm run watch` to start transpile watch. This command will read files under `client/src` and generate a single file under `client/dist/bundle.js` which should be included by index.html
---
+++ client/gulpfile.babel.js
@@ -0,0 +1,26 @@
+import gulp from "gulp";
+import browserify from "browserify";
+import source from "vinyl-source-stream";
+
+gulp.task("default", ["transpile"]);
+
+gulp.task("transpile", () => {
+
+  return browserify("src/app.js")
+    .transform("babelify")
+    .bundle()
+    .on("error", function(error){
+      console.error( "\nError: ", error.message, "\n");
+      this.emit("end");
+    })
+    .pipe(source("bundle.js"))
+    .pipe(gulp.dest("dist"));
+
+});
+
+
+
+gulp.task("watch", ["transpile"], () => {
+  gulp.watch("src/**/*", ["transpile"]);
+});
+
