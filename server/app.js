9566b69db9c71c694edb99930910295283a9b159
joeltaylor <joel@codeprivy.com>
2016-02-03 16:16:15 -0500

    Add mongo logic for indiv sport

--- server/app.js
+++ server/app.js
@@ -11,6 +11,9 @@ app.use( express.static(__dirname + "/../client") );
 app.get("/sports", (request, response) => {
   let sports = mongoUtil.sports();
   sports.find().toArray((err,docs) => {
+    if(err) {
+      response.sendStatus(400);
+    }
     console.log(JSON.stringify(docs));
     let sportNames = docs.map((sport) => sport.name);
     response.json( sportNames );
@@ -19,22 +22,16 @@ app.get("/sports", (request, response) => {
 
 app.get("/sports/:name", (request, response) => {
   let sportName = request.params.name;
-  console.log( "Sport name: ", sportName );
 
-  let sport = {
-    "name": "Cycling",
-    "goldMedals": [{
-      "division": "Men's Sprint",
-      "country": "UK",
-      "year": 2012
-    }, {
-      "division": "Women's Sprint",
-      "country": "Australia",
-      "year": 2012
-    }]
-  };
+  let sports = mongoUtil.sports();
+  sports.find({name: sportName}).limit(1).next((err,doc) => {
+    if(err) {
+      response.sendStatus(400);
+    }
+    console.log( "Sport doc: ", doc );
+    response.json(doc);
+  });
 
-  response.json(sport);
 });
 
 
