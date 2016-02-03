1e2d3020d207468e0711641f15eb98e518adb8aa
Carlos Souza <carloshrsouza@gmail.com>
2016-02-03 16:05:18 -0500

    Add /sports/:name endpoint

--- server/app.js
+++ server/app.js
@@ -17,4 +17,27 @@ app.get("/sports", (request, response) => {
   });
 });
 
+app.get("/sports/:name", (request, response) => {
+  let sportName = request.params.name;
+  console.log( "Sport name: ", sportName );
+
+  let sport = {
+    "name": "Cycling",
+    "goldMedals": [{
+      "division": "Men's Sprint",
+      "country": "UK",
+      "year": 2012
+    }, {
+      "division": "Women's Sprint",
+      "country": "Australia",
+      "year": 2012
+    }]
+  };
+
+  response.json(sport);
+});
+
+
+
+
 app.listen(8181, () => console.log( "Listening on 8181" ));
