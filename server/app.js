16baa5f39de07bde3253c87b5ab99d6cbaff2f73
joeltaylor <joel@codeprivy.com>
2016-02-03 17:14:00 -0500

    Add mongo post logic

--- server/app.js
+++ server/app.js
@@ -40,12 +40,22 @@ app.get("/sports/:name", (request, response) => {
 
 app.post("/sports/:name/medals", jsonParser, (request, response) => {
   let sportName = request.params.name;
-  let newMedal = request.body.medal;
+  let newMedal = request.body.medal || {};
 
-  console.log( "Sport name: ", sportName );
-  console.log( "Medal: ", newMedal );
+  if(!newMedal.division || !newMedal.year || !newMedal.country){
+    response.sendStatus(400);
+  }
 
-  response.sendStatus(201);
+  let sports = mongoUtil.sports();
+  let query = {name: sportName};
+  let update = {$push: {goldMedals: newMedal}};
+
+  sports.findOneAndUpdate(query, update, (err, res) => {
+    if(err){
+      response.sendStatus(400);
+    }
+    response.sendStatus(201);
+  });
 });
 
 
