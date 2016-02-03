974091e963017f239fbd2a30f6d675c1b8baf406
Alyssa Nicoll <alyssaamnell@gmail.com>
2016-02-03 15:59:48 -0500

    add ui-serf to links and list out medals

--- client/src/app.js
+++ client/src/app.js
@@ -1,7 +1,49 @@
 import angular from 'angular'
-angular.module('olympics', [])
-.controller('sportsController', function($http){
-  $http.get('/sports').then((response) => {
-    this.sports = response.data;
-  });
+import 'angular-ui-router'
+angular.module('olympics', ["ui.router"])
+
+.config(($stateProvider, $urlRouterProvider) => {
+  $urlRouterProvider.otherwise('/sports')
+
+  $stateProvider
+    .state('sports', {
+      url: '/sports',
+      templateUrl: 'sports/sports-nav.html',
+      resolve: {
+        sportsService: function($http) {
+          return $http.get('/sports');
+        }
+      },
+      controller: function(sportsService) {
+        this.sports = sportsService.data;
+      },
+      controllerAs: 'sportsCtrl'
+    })
+    .state('sports.medals', {
+      url: '/:sportName',
+      templateUrl: 'sports/sports-medals.html',
+      resolve: {
+        sportService: function($q) {
+          return $q((resolve, reject) => {
+            let sport = {
+              "name": "Cycling",
+              "goldMedals": [{
+                "division": "Men's Sprint",
+                "country": "UK",
+                "year": 2012
+              }, {
+                "division": "Women's Sprint",
+                "country": "Australia",
+                "year": 2012
+              }]
+            };
+            resolve({data: sport});
+          })
+        }
+      },
+      controller: function(sportService){
+        this.sport = sportService.data;
+      },
+      controllerAs: 'sportCtrl'
+    })
 })
