ba0948db95f89cfde0819f35e70eaf1013eee67c
Alyssa Nicoll <alyssaamnell@gmail.com>
2016-02-03 17:00:57 -0500

    add styles, create new medal form, and route

--- client/dist/bundle.js
+++ client/dist/bundle.js
@@ -36,6 +36,19 @@ _angular2.default.module('olympics', ["ui.router"]).config(function ($stateProvi
       this.sport = sportService.data;
     },
     controllerAs: 'sportCtrl'
+  }).state('sports.new', {
+    url: '/:sportName/medal/new',
+    templateUrl: 'sports/new-medal.html',
+    controller: function controller($stateParams, $state) {
+      this.sportName = $stateParams.sportName;
+
+      this.saveMedal = function (medal) {
+        console.log('medal', medal);
+
+        $state.go('sports.medals', { sportName: $stateParams.sportName });
+      };
+    },
+    controllerAs: 'newMedalCtrl'
   });
 });
 
--- client/src/app.js
+++ client/src/app.js
@@ -32,4 +32,18 @@ angular.module('olympics', ["ui.router"])
       },
       controllerAs: 'sportCtrl'
     })
+    .state('sports.new', {
+      url: '/:sportName/medal/new',
+      templateUrl: 'sports/new-medal.html',
+      controller: function($stateParams, $state){
+        this.sportName = $stateParams.sportName;
+
+        this.saveMedal = function(medal){
+          console.log('medal', medal);
+
+          $state.go('sports.medals', {sportName: $stateParams.sportName});
+        };
+      },
+      controllerAs: 'newMedalCtrl'
+    })
 })
